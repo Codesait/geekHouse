cfdd2c00b29ab8ea5350a15308733a46eda735c6
codesait <melasin4@gmail.com>
2021-06-27 13:20:09 +0100

    initial commit

---
+++ lib/pages/space.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Space extends StatefulWidget {
+  const Space({Key? key}) : super(key: key);
+
+  @override
+  _SpaceState createState() => _SpaceState();
+}
+
+class _SpaceState extends State<Space> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/utils/size.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+fullWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
+
+fullHeigth(BuildContext context) {
+  return MediaQuery.of(context).size.height;
+}
