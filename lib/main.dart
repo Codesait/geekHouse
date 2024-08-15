682c2f937e09358c39734f2457c7db8102b59914
Codesait <melasin4@gmail.com>
2024-08-15 21:31:01 +0100

    added supabase and .env for secret vars

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:projects/app/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 final GlobalKey<NavigatorState> appNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
 
-void main() {
+const anonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6d29md3Z6bG1zeWFqbml3eGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM3NDU4MzQsImV4cCI6MjAzOTMyMTgzNH0.2ckGkkjEVfm3mbYsmIzuPfFKYmrCm9tuJY8rwQV5rgQ';
+
+Future<void> main() async {
+  await dotenv.load(fileName: 'auth_secrets.env');
+
+  final supabaseUrl = dotenv.get('AUTH_URL');
+  final anonKey = dotenv.get('ANON_KEY');
+
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: anonKey,
+    realtimeClientOptions: const RealtimeClientOptions(
+      logLevel: RealtimeLogLevel.info,
+    ),
+    storageOptions: const StorageClientOptions(
+      retryAttempts: 10,
+    ),
+  );
   runApp(const MyApp());
 }
